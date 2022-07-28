fead52cb1094503d4423dcff9db924cada0c241c
Matthias <75242607+moutth@users.noreply.github.com>
2022-07-28 11:08:47 +0200

    send input is disabled when no port is open

--- command.js
+++ command.js
@@ -6,6 +6,8 @@ const sendBtn = $("#sendBtn");
 const encoder = new TextEncoder();
 
 $(() => {
+    disableSend();
+
     sendInput.on("keyup", (e) => {
         if (e.key == "Enter") {
             send(sendInput.val());
@@ -21,4 +23,14 @@ $(() => {
 
 async function send(stringToSend){
     await port.write(encoder.encode(stringToSend));
+}
+
+function enableSend(){
+    sendInput.attr("disabled", false);
+    sendBtn.attr("disabled", false);
+}
+
+function disableSend(){
+    sendInput.attr("disabled", true);
+    sendBtn.attr("disabled", true);
 }
