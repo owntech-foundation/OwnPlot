20b5921e8f70c782fabd02883a9c3e62cffdb2d3
guigur <guigur13@gmail.com>
2022-08-05 17:10:11 +0200

    add, remove, and update functions for commands buttons

--- scripts/command.js
+++ scripts/command.js
@@ -3,6 +3,14 @@ const sendBtn = $("#sendBtn");
 
 const encoder = new TextEncoder();
 
+let commandButtons = [
+    {
+        color: "#ff0000",
+        text: "test button",
+        command: "t"
+    }
+];
+
 $(() => {
     disableSend();
 
@@ -17,18 +25,41 @@ $(() => {
         send(sendInput.val());
         console.log('sent---> ' + sendInput.val());
     });
+    updateCommandButtons();
 });
+
+function addCommandButton() {
+    let newButton = {
+        color: "#ff00ff",
+        text: "test button",
+        command: "t"
+    }
+    commandButtons.push(newButton);
+    updateCommandButtons();
+}
+
+function removeCommandButton(index) {
+    commandButtons.splice(index, 1);
+    updateCommandButtons();
+}
+
+function updateCommandButtons() {
+    $("#commandButtonContainer").empty();
+    commandButtons.forEach((elem, index) => {
+        $("#commandButtonContainer").append('<button type="button" class="btn btn-primary commandButton" id="cmdBtn-' + index + '" >' + elem.text + '</button>');
+    });
+}
 
 async function send(stringToSend){
     await port.write(encoder.encode(stringToSend));
 }
 
-function enableSend(){
+function enableSend() {
     sendInput.attr("disabled", false);
     sendBtn.attr("disabled", false);
 }
 
-function disableSend(){
+function disableSend() {
     sendInput.attr("disabled", true);
     sendBtn.attr("disabled", true);
 }
