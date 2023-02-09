5af36b0dab38e3bade8c588a8af8f4417ea79a9b
Jean Alinei <jean.alinei@laas.fr>
2023-02-09 15:59:57 +0100

    Added a vertical dragbar
    Signed-off-by: Jean Alinei <jean.alinei@laas.fr>

---
+++ scripts/dragBar.js
@@ -0,0 +1,14 @@
+/**
+ * @ Licence: OwnPlot, the OwnTech data plotter. Copyright (C) 2022. Matthias Riffard & Guillaume Arthaud - OwnTech Foundation.
+	Delivered under GNU Lesser General Public License Version 2.1 (https://opensource.org/licenses/LGPL-2.1)
+ * @ Website: https://www.owntech.org/
+ * @ Mail: owntech@laas.fr
+ * @ Create Time: 2022-08-30 09:31:24
+ * @ Modified by: Your name
+ * @ Modified time: 2022-09-07 13:44:23
+ * @ Description:
+ */
+
+$(() => {   
+	Split(['#sideBar', '#chartAndTerminalDiv']);
+});
